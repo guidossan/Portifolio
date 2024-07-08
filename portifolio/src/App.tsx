9409e6b8969f575de6c1c0b82e1074b5de8b1ce2
Guilherme Martins <guimsl2004@gmail.com>
2024-07-08 15:39:00 -0300

    teste inicial

---
+++ portifolio/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="bg-sky-500">
+      Teste
+    </div>
+  );
+}
+
+export default App;

452c4db452bb376dfb8d1d6a6722f260baea8077
Guilherme Martins <guimsl2004@gmail.com>
2024-07-08 18:06:31 -0300

    teste 2 background images

--- portifolio/src/app/page.tsx
+++ portifolio/src/app/page.tsx
@@ -7,6 +7,7 @@ export default function Home() {
   return (
     <main className="flex flex-col items-center fustify-between">
       <div className="relative w-full">
+
         <div className="absolute -z-10 w-full">
           <Image src={img2} alt="backgoundImage" className="w-full"/>
         </div>
@@ -14,8 +15,19 @@ export default function Home() {
         <div className="max-w-screen-lg m-auto h-[200vh] py-5 px-5 my-10 text-center text-2xl rounded-lg bg-white shadow-[0px_8px_48px_rgba(0,0,0,0.25)]">
           <p className="text-black">Como colocar imagem de fundo</p>
         </div>
-        
       </div>
+      
+      <div className="relative w-full">
+
+        <div className="absolute -z-10 w-full">
+          <Image src={img1} alt="backgoundImage" className="w-full"/>
+        </div>
+
+        <div className="max-w-screen-lg m-auto h-[200vh] py-5 px-5 my-10 text-center text-2xl rounded-lg bg-white shadow-[0px_8px_48px_rgba(0,0,0,0.25)]">
+          <p className="text-black">Como colocar imagem de fundo</p>
+        </div>
+      </div>
+      
     </main>
    
    

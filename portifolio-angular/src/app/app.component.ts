a4a8017526aa4199e9566a23ce413cf84ea1850d
Guilherme Martins <guimsl2004@gmail.com>
2025-01-16 08:33:26 -0300

    limpa codigo inicial, rota para home

--- portifolio-angular/src/app/app.component.ts
+++ portifolio-angular/src/app/app.component.ts
@@ -1,12 +1,14 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  imports: [RouterOutlet, CommonModule],
+  template: `
+    <router-outlet />
+    `,
 })
 export class AppComponent {
-  title = 'portifolio-angular';
+  
 }

88a440664f812dcaf9828506269986b7c62a1139
Guilherme Martins <guimsl2004@gmail.com>
2025-02-10 15:32:52 -0300

    refatoração para componetização do sobre

--- src/app/modules/portifolio/components/inicial/inicial.component.ts
+++ src/app/modules/portifolio/components/inicial/inicial.component.ts
@@ -3,11 +3,12 @@ import { Component } from '@angular/core';
 import { SobreComponent } from '../sobre/sobre.component';
 import { BotoesComecoComponent } from "../botoes-comeco/botoes-comeco.component";
 import { ProjetosComponent } from "../projetos/projetos.component";
+import { ServicosComponent } from "../servicos/servicos.component";
 
 @Component({
   selector: 'app-inicial',
   standalone: true,
-  imports: [CommonModule, BotoesComecoComponent, ProjetosComponent],
+  imports: [CommonModule, BotoesComecoComponent, ProjetosComponent, SobreComponent, ServicosComponent],
   templateUrl: './inicial.component.html',
   styleUrl: './inicial.component.scss'
 })

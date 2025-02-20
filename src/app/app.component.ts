14cea98238f3e5a022c1656a47282f670cc7677f
Guilherme Martins <guimsl2004@gmail.com>
2025-02-20 15:09:16 -0300

    ssr no portifólio com keywords

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,7 @@ import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { TranslateModule } from '@ngx-translate/core';
-
+import { Title, Meta } from '@angular/platform-browser';
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet, CommonModule,TranslateModule],
@@ -10,6 +10,15 @@ import { TranslateModule } from '@ngx-translate/core';
     <router-outlet />
     `,
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
+  constructor(private titleService: Title, private metaService: Meta) {}
 
+  ngOnInit() {
+    this.titleService.setTitle('Meu Portfólio Angular');
+    this.metaService.addTags([
+      { name: 'description', content: 'Este é o meu portfólio desenvolvido em Angular.' },
+      { name: 'author', content: 'Guilherme Martins dos Santos Lorenzini' },
+      { name: 'keywords', content: 'Criar aplicações web, Portifólio de programador web, Programador web, Programador full-stack, Desenvolvedor web, Desenvolvedor full-stack, Contratar desenvolvedor, Desenvolvedor para projetos web' }
+    ]);
+  }
 }
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -7,6 +7,7 @@ import { provideHttpClient } from '@angular/common/http';
 import { provideTranslate } from './app.translate';
 import LocalePt from '@angular/common/locales/pt';
 import { registerLocaleData } from '@angular/common';
+import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
 registerLocaleData(LocalePt);
 
 
@@ -16,5 +17,30 @@ export const appConfig: ApplicationConfig = {
     //toda requisição passa por ele
     provideHttpClient(),
     provideTranslate(),
-    { provide: LOCALE_ID, useValue: 'pt-BR' }]
+    { provide: LOCALE_ID, useValue: 'pt-BR' },
+    provideClientHydration(
+      withEventReplay(),
+      //guarda no cache requisição post
+      withHttpTransferCacheOptions({
+        includePostRequests: true,
+      })
+    )
+    ]
 };
+
+/*
+  Sem SSR (Client-Side Rendering - CSR)
+    O navegador baixa os arquivos estáticos (HTML, CSS, JavaScript).
+    O Angular renderiza a aplicação no cliente.
+    Navegação ocorre no cliente sem recarregar a página.
+
+  Com SSR (Server-Side Rendering)
+    O servidor gera e envia o HTML já renderizado.
+    O Angular no cliente "hidrata" a aplicação, tornando-a interativa.
+    Navegação subsequente ocorre no cliente, sem necessidade de nova renderização no servidor.
+
+  Benefícios do SSR
+  ✅ Melhor SEO (conteúdo já renderizado para motores de busca).
+  ✅ Melhor tempo de carregamento inicial.
+  ✅ Melhor experiência do usuário com interatividade mais rápida.
+*/
